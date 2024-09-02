a094bd49fa364aac389e53b4ed71b552e1026c68
Gurasshrimp <jack04309487@gmail.com>
2024-09-02 20:39:32 +0800

    feat: 改進音樂播放功能
    這個 commit 改進了音樂播放功能，修復了一個 bug，並且加入了錯誤處理。現在，當機器人播放音樂時，如果遇到錯誤，它會顯示錯誤訊息。這個改變可以提升使用者體驗和程式的穩定性。

--- cogs/music.py
+++ cogs/music.py
@@ -17,11 +17,14 @@ class Music(commands.Cog):
         if not voice:
             await interaction.followup.send(f'機器人不在頻道內，請考慮使用/join')
         else:
-            yt = YouTube(url)
-            yt.streams.get_audio_only().download(filename='song',mp3=True)
+            try:
+                yt = YouTube(url)
+                yt.streams.get_audio_only().download(filename='song',mp3=True)
 
-            voice.play(discord.FFmpegPCMAudio(source='song.mp3',executable='ffmpeg/bin/ffmpeg.exe'))
-            await interaction.followup.send(f'正在播放: [{yt.title}]({url}d)')
+                voice.play(discord.FFmpegPCMAudio(source='song.mp3',executable='ffmpeg/bin/ffmpeg.exe'))
+                await interaction.followup.send(f'正在播放: [{yt.title}]({url}d)')
+            except Exception as e:
+                print(e)
 
     @app_commands.command(description='加入使用者目前頻道')
     async def join(self,interaction:discord.Interaction):
