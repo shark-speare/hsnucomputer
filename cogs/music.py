3eaf575f94d1c9f26e2bad326484af397a95d14a
Gurasshrimp <jack04309487@gmail.com>
2024-09-04 21:32:11 +0800

    改進音樂播放功能，修復 bug，加入錯誤處理

--- cogs/music.py
+++ cogs/music.py
@@ -18,13 +18,13 @@ class Music(commands.Cog):
             await interaction.followup.send(f'機器人不在頻道內，請考慮使用/join')
         else:
             try:
+                
                 yt = YouTube(url,use_oauth=True)
-                yt.streams.get_audio_only().download(filename='song',mp3=True)
-
-                voice.play(discord.FFmpegPCMAudio(source='song.mp3',executable='ffmpeg/bin/ffmpeg.exe'))
+                yt.streams.get_audio_only().download(filename=str(interaction.guild_id),mp3=True)
+                voice.play(discord.FFmpegPCMAudio(source=f'{interaction.guild_id}.mp3'))
                 await interaction.followup.send(f'正在播放: [{yt.title}]({url}d)')
             except Exception as e:
-                print(e)
+                await print(e)
 
     @app_commands.command(description='加入使用者目前頻道')
     async def join(self,interaction:discord.Interaction):
