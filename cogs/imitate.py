0ea00ec529b531b8e650539e4b6008cd6e25a368
Gurasshrimp <jack04309487@gmail.com>
2024-10-03 17:01:49 +0800

    偷偷模仿別人說話

---
+++ cogs/imitate.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import requests
+
+class Imitate(commands.Cog):
+    def __init__(self,bot:commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def imitate(self,ctx:commands.Context,member_id:int,content:str):
+        
+        member = await ctx.guild.fetch_member(member_id)
+        avatar_url = member.display_avatar.url
+        name = member.display_name
+        
+
+        webhook = await ctx.channel.create_webhook(name='imitate')
+        await ctx.channel.purge(limit=1)
+        await webhook.send(avatar_url=avatar_url,username=name,content=content)
+        await webhook.delete()
+
+
+
+async def setup(bot):
+    await bot.add_cog(Imitate(bot))
