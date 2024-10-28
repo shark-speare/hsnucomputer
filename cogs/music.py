3a8e57303e2b3010a429816990391f76622632a0
Gurasshrimp <jack04309487@gmail.com>
2024-10-28 14:39:11 +0800

    新增queue指令，修改確認語音頻道的架構，並且把取得資訊提前至play指令

--- cogs/music.py
+++ cogs/music.py
@@ -17,58 +17,72 @@ class Music(commands.Cog):
             })
         self.next = False
     
-    queue = {int:list}
+        self.queue = {int:list}
 
     @app_commands.command(description='加入頻道並播放Youtube音訊')
     @app_commands.describe(網址或關鍵字='可指定創作者來準確搜尋，也可直接輸入網址')
     async def play(self,interaction:discord.Interaction,網址或關鍵字:str):
-        
+        await interaction.response.defer()
         # 確保機器人在語音頻道內
-        voice:Optional[discord.VoiceClient] = discord.utils.get(self.bot.voice_clients,guild=interaction.guild)
-        if not voice:
-            channel = interaction.user.voice.channel
-            if not channel:
-                await interaction.response.send_message('請先加入一個語音頻道')
-                return
-            voice = await channel.connect()
-            voice:discord.VoiceClient = discord.utils.get(self.bot.voice_clients,guild=interaction.guild)
+        voice = await self.check_in_voice(interaction)
+        if voice == 0: return
 
         # 將歌曲加入清單，如果清單不存在就創建
         if interaction.guild.id not in self.queue:
             self.queue[interaction.guild.id] = []
-        self.queue[interaction.guild.id].append(網址或關鍵字)
-        await interaction.response.send_message('歌曲已加入隊伍')
+
+        # 依據輸入內容選擇取得方式
+        if 'https://' in 網址或關鍵字:
+            info = self.downloader.extract_info(網址或關鍵字,download=False)
+        else:
+            info = self.downloader.extract_info(f"ytsearch:{網址或關鍵字}",download=False)['entries'][0]
+        
+        self.queue[interaction.guild.id].append(
+            {
+                'title': info['title'],
+                'url': info['url']
+            }
+        )
+        
+        await interaction.followup.send(f"{info['title']}已加入隊伍")
 
         # 如果沒有在播放就開始播放
         if not voice.is_playing():
             await self.play_next(interaction)
-        
 
     async def play_next(self,interaction:discord.Interaction):
         if len(self.queue[interaction.guild.id]) > 0:
             
             # 取得歌曲資源並移除隊伍
-            query = self.queue[interaction.guild.id][0]
-            if 'https://' in query:
-                info = self.downloader.extract_info(query,download=False)
-            else:
-                info = self.downloader.extract_info(f"ytsearch:{query}",download=False)['entries'][0]
-
-            url = info['url']
+            song:dict = self.queue[interaction.guild.id][0]
             self.queue[interaction.guild.id].pop(0)
             
             #開播
-            source = discord.FFmpegPCMAudio(source=url,executable='ffmpeg')
+            source = discord.FFmpegPCMAudio(source=song['url'],executable='ffmpeg')
             voice:discord.VoiceClient = discord.utils.get(self.bot.voice_clients,guild=interaction.guild)
             voice.play(source,after=lambda _:self.bot.loop.create_task(self.play_next(interaction)))
-            await interaction.followup.send(f'正在播放{info["title"]}')
+            await interaction.followup.send(f"正在播放{song['title']}")
+            
+            # 處理/next指令
             self.next = False
         else:
             if not self.next:
                 await interaction.followup.send('播放完畢')
             else:
                 self.next = False
-        
+    
+    async def check_in_voice(self,interaction:discord.Interaction) -> discord.VoiceClient|int: 
+        voice:Optional[discord.VoiceClient] = discord.utils.get(self.bot.voice_clients,guild=interaction.guild)
+        if not voice:
+            user_voice = interaction.user.voice
+            if not user_voice:
+                await interaction.response.send_message('請先加入一個語音頻道')
+                return 0
+            voice = await user_voice.channel.connect()
+            voice:discord.VoiceClient = discord.utils.get(self.bot.voice_clients,guild=interaction.guild)
+
+        return voice
+
     @app_commands.command(description='跳下一首')
     async def next(self,interaction:discord.Interaction):
         await interaction.response.defer()
@@ -106,7 +120,6 @@ class Music(commands.Cog):
         voice.resume()
         await interaction.response.send_message('音樂已繼續播放')
 
-
     @app_commands.command(description='離開語音頻道')
     async def leave(self, interaction: discord.Interaction):
         voice: Optional[discord.VoiceClient] = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
@@ -117,6 +130,14 @@ class Music(commands.Cog):
         await voice.disconnect()
         await interaction.response.send_message('已離開語音頻道')
 
+    @app_commands.command(description='查看當前隊列')
+    async def queue(self,interaction:discord.Interaction):
+        await interaction.response.defer()
+        if len(self.queue[interaction.guild.id]) == 0:
+            interaction.followup.send('隊列無音樂')
+            return
+        queue_list = [song['title'] for song in self.queue[interaction.guild.id]]
+        await interaction.followup.send("`" + "`\n`".join(queue_list)+"`")
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(Music(bot))
