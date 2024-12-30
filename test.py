c7284aee06b3f015482f377b7ae4804e7e1b438a
shark-speare <jack04309487@gmail.com>
2024-12-30 13:20:52 +0800

    轉換玩家資料格式

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-from yt_dlp import YoutubeDL
 import json
 
-dl = YoutubeDL({
-    # 'username' : 'oauth',
-    # 'password' : ""
-})
 
-info = dl.extract_info('https://www.youtube.com/watch?v=IA19lRCi4yE',download=False)
 
-with open('result.json',mode='w',encoding='utf8') as file:
-    json.dump(info, file, ensure_ascii=False, indent=4)
+
+with open('playerData.json',mode='r+',encoding='utf8') as file:
+    data:dict = json.load(file)
+
+    for i in data.keys():
+        id = data[i]['status']['doing']
+        time = data[i]['status']["workStartTimestamp"]
+        data[i]['status']['doing'] = {
+            "id" : id,
+            "useItems" : [],
+            "startTimestamp" : time,
+
+        }
+    file.seek(0)
+    file.truncate()
+
+    json.dump(data,file, ensure_ascii=False, indent=4)
