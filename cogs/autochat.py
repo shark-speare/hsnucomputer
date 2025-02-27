a142366365985b1df353d54dc64629c453bc87c0
shark-speare <jack04309487@gmail.com>
2025-02-27 12:59:47 +0800

    不要印出來

--- cogs/autochat.py
+++ cogs/autochat.py
@@ -22,8 +22,6 @@ class Chat(commands.Cog):
             msgs.append((msg.author.name, msg.content))
         msgs.reverse()
 
-        print(msgs)
-
         client = genai.Client(api_key=self.key)
 
         prompt = f"這些是一串聊天記錄組成的清單。每一項有一個元組，是一則訊息，包含了發言者名稱與內容，請你扮演「要不要管電機」這個角色來聊天，請說出下一句話，不需要包含角色身份等等，只要說話的內容就好: {msgs}"
