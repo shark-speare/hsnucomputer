66a26e1a3ce7d6e02371fb6c762ba642ee11c62e
shark-speare <jack04309487@gmail.com>
2025-02-27 12:53:58 +0800

    自動聊天

--- cogs/autochat.py
+++ cogs/autochat.py
@@ -27,7 +27,7 @@ class Chat(commands.Cog):
         prompt = f"這些是一串聊天記錄組成的清單。每一項有一個元組，是一則訊息，包含了發言者名稱與內容，請你扮演「要不要管電機」這個角色來聊天，請說出下一句話: {msgs}"
 
         response = client.models.generate_content(
-            model="gemini-flash-2.0",
+            model="gemini-2.0-flash",
             contents=prompt
         )
 
