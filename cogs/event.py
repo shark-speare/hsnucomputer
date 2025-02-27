d3136620620bd24dfa6360dab19a265d9b67b7e9
shark-speare <jack04309487@gmail.com>
2025-02-27 09:09:39 +0800

    尷尬化

--- cogs/event.py
+++ cogs/event.py
@@ -19,7 +19,7 @@ class Event(commands.Cog):
         print(1)
         self.runEvents.start()
 
-    @tasks.loop(hours=1)
+    @tasks.loop(minutes=15)
     async def runEvents(self):
         channel = await self.bot.fetch_channel(1281252737182601273)
         msgs = channel.history(limit=1)
@@ -39,19 +39,9 @@ class Event(commands.Cog):
 
             client = genai.Client(api_key=self.key)
 
-            prompt = """請給我2個貼近生活、適合討論的話題
-                例如
-                "早上起床先刷牙還是先吃早餐",
-                "洗澡時會先洗頭還是先洗身體",
-                "喝珍珠奶茶會咬珍珠嗎",
-                "吃飯先吃菜還是先吃肉",
-                "覺得漢堡該不該切開吃",
-                "睡覺時需要完全黑暗嗎",
-                "喝湯前會先吹嗎",
-                "覺得豆腐花該吃甜的還是鹹的",
-                然後想像你在一個多人聊天室裡面發起一個新的話題
-                所以不需要很正式的格式，只要很輕鬆的隨意提起就好
-                以此情境回覆"""
+            prompt = """請假設你在聊天室，請開一個話題
+                        越尷尬越好
+                        但尷尬的不是話題，而是說話的方式，話題可以正常"""
             
             response = client.models.generate_content(
                 model="gemini-2.0-flash",
