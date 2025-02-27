3dddcb34ef7584800697ee1ce10c2633664060a2
shark-speare <jack04309487@gmail.com>
2025-02-27 12:52:55 +0800

    測試

---
+++ cogs/autochat.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+import os
+load_dotenv()
+from google import genai
+from pydantic import BaseModel
+
+
+
+class Chat(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+        self.key: str = os.environ['gemini']        
+    
+    @commands.Cog.listener()
+    async def on_message(self, msg:discord.Message):
+        if msg.channel.id != 1344529522019532951: return
+
+        msgs = []
+        async for msg in msg.channel.history(limit=10):
+            msgs.append((msg.author.name, msg.content))
+        msgs.reverse()
+
+        client = genai.Client(api_key=self.key)
+
+        prompt = f"這些是一串聊天記錄組成的清單。每一項有一個元組，是一則訊息，包含了發言者名稱與內容，請你扮演「要不要管電機」這個角色來聊天，請說出下一句話: {msgs}"
+
+        response = client.models.generate_content(
+            model="gemini-flash-2.0",
+            contents=prompt
+        )
+
+        await msg.channel.send(response)
+
+async def setup(bot:commands.Bot):
+    await bot.add_cog(Chat(bot))        
+    
--- test.py
+++ test.py
@@ -5,12 +5,21 @@ from google import genai
 key = os.environ['gemini']
 
 
+
+
 client = genai.Client(api_key=key)
 
-prompt = [
-"""請假設你在聊天室，請開一個話題
-越尷尬越好
-但尷尬的不是話題，而是說話的方式，話題可以正常"""]
+prompt = ["""請給出1項可討論的問題以及幾個選項，可以貼近生活，如喜不喜歡香菜、幾點睡覺、回家會先脫襪子嗎、在家工作會不會換衣服
+使用以下schema:
+{
+    question: str
+    content: {
+            text: str
+            emoji: str
+        }
+    }
+不需要有任何說明，我只要字典字串，也不要```json```
+"""]
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
