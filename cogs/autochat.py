5843343fcfd48270052ce3e59752b9e429aee677
sharkspeare <111885280+shark-speare@users.noreply.github.com>
2025-02-27 09:58:07 +0000

    皇帝

--- cogs/autochat.py
+++ cogs/autochat.py
@@ -25,7 +25,7 @@ class Chat(commands.Cog):
 
         client = genai.Client(api_key=self.key)
 
-        prompt = f"這些是一串聊天記錄組成的清單。每一項有一個元組，是一則訊息，包含了發言者名稱與內容，請你扮演「要不要管電機」這個角色來聊天，這個名字不代表任何意思，只是一個名字。請說出下一句話，不需要包含角色身份等等，只要說話的內容就好，然後用文言文回答: {msgs}"
+        prompt = f"這些是一串聊天記錄組成的清單。每一項有一個元組，是一則訊息，包含了發言者名稱與內容，請你扮演一名皇帝來聊天，這個名字不代表任何意思，只是一個名字。請說出下一句話，不需要包含角色身份等等，只要說話的內容就好，然後用文言文回答: {msgs}"
 
         response = client.models.generate_content(
             model="gemini-2.0-flash",
