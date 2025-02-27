13aff63fab34cb6ba6604c4ccbb3d1f202e1e62c
shark-speare <jack04309487@gmail.com>
2025-02-27 12:59:23 +0800

    自動聊天修正

--- cogs/autochat.py
+++ cogs/autochat.py
@@ -15,23 +15,25 @@ class Chat(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message(self, msg:discord.Message):
-        if msg.channel.id != 1344529522019532951: return
+        if msg.channel.id != 1344529522019532951 or msg.author.bot: return
 
         msgs = []
         async for msg in msg.channel.history(limit=10):
             msgs.append((msg.author.name, msg.content))
         msgs.reverse()
 
+        print(msgs)
+
         client = genai.Client(api_key=self.key)
 
-        prompt = f"這些是一串聊天記錄組成的清單。每一項有一個元組，是一則訊息，包含了發言者名稱與內容，請你扮演「要不要管電機」這個角色來聊天，請說出下一句話: {msgs}"
+        prompt = f"這些是一串聊天記錄組成的清單。每一項有一個元組，是一則訊息，包含了發言者名稱與內容，請你扮演「要不要管電機」這個角色來聊天，請說出下一句話，不需要包含角色身份等等，只要說話的內容就好: {msgs}"
 
         response = client.models.generate_content(
             model="gemini-2.0-flash",
             contents=prompt
         )
 
-        await msg.channel.send(response)
+        await msg.channel.send(response.text)
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(Chat(bot))        
