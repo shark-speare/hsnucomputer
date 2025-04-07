2a69dbfa78c2b2c37e0522ce14d368eadc60c87e
shark-speare <jack04309487@gmail.com>
2025-04-07 14:02:37 +0800

    cookie4

--- cogs/music.py
+++ cogs/music.py
@@ -57,7 +57,7 @@ class Music(commands.Cog):
         
         ydl = YoutubeDL({
             "format": 'bestaudio/best',
-            "cookiefile": "../cookies.txt",
+            "cookiefile": "./cookies.txt",
             })
         
         # 網址
