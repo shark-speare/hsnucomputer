bfb6bb266edfcdb1e08750681cbe81b465437f24
shark-speare <jack04309487@gmail.com>
2025-06-09 10:06:43 +0800

    調整取得公告方式，新增日期

--- cogs/hsnu.py
+++ cogs/hsnu.py
@@ -1,83 +1,72 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-import feedparser
-from discord import SelectOption
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+import requests
+from discord import ui
 
 class Hsnu(commands.Cog):
-    def __init__(self,bot:commands.Bot):
+    def __init__(self, bot:commands.Bot):
         self.bot = bot
 
-    @app_commands.command(description='取得師大附中最新公告')
-    async def announcment(self,interaction:discord.Interaction):
-        await interaction.response.defer()
+    @app_commands.command()
+    async def announcement(self, interaction:discord.Interaction):
+        await interaction.response.defer() # 一定要進思考，因為取得資料需要時間
 
-        #取得rss feed非置頂的前5項
-        url = 'https://www.hs.ntnu.edu.tw/rssfeeds?a=T0RESTEyNTIxODAyNTk2MjI2MVRDaW50ZWx5&b=T0RESTYyaW50ZWx5&c=T0RESU1EVTNOakl5TXpjPXdBek55SWpOeElrVGludGVseQ=='
-        content:list = feedparser.parse(url)['entries']
-    
-        titles = [article['title'] for article in content]
+        feed = requests.get('https://www.hs.ntnu.edu.tw/rssfeeds?a=T0RESTEyNTIxODAyNTk2MjI2MVRDaW50ZWx5&b=T0RESTYyaW50ZWx5&c=T0RESU1EVTNOakl5TXpjPXdBek55SWpOeElrVGludGVseQ==').text
 
-        choice = Title(titles, interaction.user)
-        view = discord.ui.View(timeout=60)
-        view.add_item(choice)
+        soup = BeautifulSoup(feed, 'xml')
+        items = soup.find_all('item') # 取得所有文章的列表
+
+        view = ui.View()
+        view.add_item(Select(items))
 
         await interaction.followup.send(view=view)
 
-        timeout = await view.wait()
-        if timeout:
-            await interaction.edit_original_response(content="請求過久，請重新使用指令", view=None)
-            return
+def extract(item):
+    
+    soup = BeautifulSoup(item.description.text, 'html.parser')
+    for br in soup.find_all('br'):
+        br.replace_with('\n')
 
-        title = view.children[0].values[0]
+    return(soup.text)
 
-        article = list(filter(lambda x:x['title']==title, content))[0]
+class Select(ui.Select):
+    def __init__(self, items):
+        super().__init__(
+            placeholder="請選擇一個標題",
+            min_values=1,
+            max_values=1
+        )
+
+        self.items = items # 先存到屬性裡面，因為callback需要
         
-        datetime = article['published']
+        for index, item in enumerate(items):
+            # 把label設為標題，value則設為位置(value的要求是str所以我們轉一下格式)
+            self.add_option(label=item.title.text, value=str(index), description=item.pubDate.text) 
 
-        parser = Handler()
-        parser.feed(article['summary'])
-        
-        content = "".join(parser.result)
-
-        view = discord.ui.View()
-        view.add_item(Link(article['links'][0]['href']))
-
-        await interaction.edit_original_response(content="\n".join(("## "+title, datetime+"\n", content)), view=view)
-
-
-        
-class Title(discord.ui.Select):
-    def __init__(self, titles:list, user):
-        super().__init__(min_values=1, max_values=1, placeholder="選擇一個標題")
-        self.user = user
-        for title in titles:
-            self.add_option(label=title, value=title)
-
-    async def callback(self,interaction):
-        if interaction.user != self.user:
-            await interaction.response.send_message("這不是你的選單", ephemeral=True)
-            return
-
+    async def callback(self, interaction:discord.Interaction):
         await interaction.response.defer()
-        self.disabled=True
-        await interaction.message.edit(content="正在取得內容，請稍候", view=self.view)
-        self.view.stop()
 
-class Link(discord.ui.Button):
-    def __init__(self, link:str):
-        super().__init__(label="點我前往本文", url=link)
+        # 取得使用者選的公告索引值，然後從self.items裡面找出來
+        item = self.items[int(self.values[0])] # value是字串，把它轉回整數
+        content = extract(item)
 
-class Handler(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.result = []
+        # 用三個引號組成的字串可以像這樣換行寫
+        # 記得字串裡面不用考慮縮排，所以要頂到最旁邊寫
+        message = f"""
+## {item.title.text}
+{item.pubDate.text}
 
-    def handle_data(self, data):
-        self.result.append(data)
-        
+{content}
+""".strip() # 這個函數可以移除多餘的換行
+        view = ui.View()
+        view.add_item(Link(item.link.text))
+        await interaction.message.edit(content=message, view=view)
 
+class Link(ui.Button):
+    def __init__(self, link):
+        super().__init__(label="點我前往原文", url=link)
 
 async def setup(bot):
     await bot.add_cog(Hsnu(bot))
